eccbe19f5400aa35679395c42590d3f0c17e053b
mafanghua <980992720@qq.com>
2018-02-04 19:39:31 +0800

    no message

---
+++ WeiDeCar/Module/Login/WDAccountRegisterViewController.m
@@ -0,0 +1,25 @@
+//
+//  WDAccountRegisterViewController.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/4.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDAccountRegisterViewController.h"
+
+@interface WDAccountRegisterViewController ()
+
+@end
+
+@implementation WDAccountRegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
