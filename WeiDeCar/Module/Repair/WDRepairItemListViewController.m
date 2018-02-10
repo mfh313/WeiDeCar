9924f8d379740e6a8c3e3e5bd1f39e86fe935030
mafanghua <980992720@qq.com>
2018-02-10 17:58:40 +0800

    no message

--- WeiDeCar/Module/Repair/WDRepairItemListViewController.m
+++ WeiDeCar/Module/Repair/WDRepairItemListViewController.m
@@ -8,10 +8,11 @@
 
 #import "WDRepairItemListViewController.h"
 #import "WDListRepairTaskByUserApi.h"
+#import "WDDiagnoseModel.h"
 
 @interface WDRepairItemListViewController ()
 {
-    
+    NSMutableArray *m_repairItems;
 }
 
 @end
@@ -23,6 +24,69 @@
     
     self.title = @"维修任务列表";
     [self setBackBarButton];
+    
+    [self getListRepairTaskByUser];
+}
+
+-(void)getListRepairTaskByUser
+{
+    WDLoginService *loginService = [[MMServiceCenter defaultCenter] getService:[WDLoginService class]];
+    
+    __weak typeof(self) weakSelf = self;
+    WDListRepairTaskByUserApi *mfApi = [WDListRepairTaskByUserApi new];
+    mfApi.currentUser = loginService.currentUserInfo;
+    
+    [mfApi startWithCompletionBlockWithSuccess:^(YTKBaseRequest * request) {
+        
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        if (!mfApi.messageSuccess) {
+            [strongSelf showTips:mfApi.errorMessage];
+            return;
+        }
+        
+        NSArray *responseNetworkData = mfApi.responseNetworkData;
+        NSMutableArray *tempArray = [NSMutableArray array];
+        
+        for (int i = 0; i < responseNetworkData.count; i++) {
+            WDDiagnoseModel *itemModel = [WDDiagnoseModel yy_modelWithDictionary:responseNetworkData[i]];
+            [tempArray addObject:itemModel];
+        }
+        
+        m_repairItems = tempArray;
+        
+        [strongSelf reloadTableView];
+        
+    } failure:^(YTKBaseRequest * request) {
+        //        NSString *errorDesc = [NSString stringWithFormat:@"错误状态码=%@\n错误原因=%@",@(request.requestOperationError.code),[request.requestOperationError localizedDescription]];
+        //        [self showTips:errorDesc];
+    }];
+}
+
+-(void)reloadTableView
+{
+    [self makeCellObjects];
+//    [m_tableView reloadData];
+}
+
+-(void)makeCellObjects
+{
+    [m_cellInfos removeAllObjects];
+    
+//    for (int i = 0; i < m_diagnoseArray.count; i++) {
+//        WDDiagnoseModel *itemModel  = m_diagnoseArray[i];
+//
+//        MFTableViewCellObject *diagnoseModel = [MFTableViewCellObject new];
+//        diagnoseModel.cellHeight = 150.0f;
+//        diagnoseModel.cellReuseIdentifier = @"diagnoseItem";
+//        diagnoseModel.attachIndex = i;
+//        [m_cellInfos addObject:diagnoseModel];
+//
+//        MFTableViewCellObject *separator = [MFTableViewCellObject new];
+//        separator.cellHeight = MFOnePixHeight;
+//        separator.cellReuseIdentifier = @"separator";
+//        separator.attachIndex = i;
+//        [m_cellInfos addObject:separator];
+//    }
 }
 
 - (void)didReceiveMemoryWarning {
