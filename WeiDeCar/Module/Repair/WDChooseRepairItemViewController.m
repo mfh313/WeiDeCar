a2d95e553f3a9f8dd9d19955e544ce2e4dff1dbf
mafanghua <980992720@qq.com>
2018-02-10 21:51:48 +0800

    no message

--- WeiDeCar/Module/Repair/WDChooseRepairItemViewController.m
+++ WeiDeCar/Module/Repair/WDChooseRepairItemViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "WDChooseRepairItemViewController.h"
+#import "WDListRepairItemOffersApi.h"
+#import "WDRepairItemOfferModel.h"
 
-@interface WDChooseRepairItemViewController ()
+@interface WDChooseRepairItemViewController () <UITableViewDataSource,UITableViewDelegate>
 {
+    MFUITableView *m_tableView;
     
+    NSMutableArray<WDRepairItemOfferModel *> *m_repairItemOffers;
 }
 
 @end
@@ -22,6 +26,173 @@
     
     self.title = @"维修报价表";
     [self setBackBarButton];
+    
+    m_tableView = [[MFUITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    m_tableView.backgroundColor = [UIColor hx_colorWithHexString:@"f5f5f5"];
+    m_tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    m_tableView.dataSource = self;
+    m_tableView.delegate = self;
+    [self.view addSubview:m_tableView];
+    [m_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top);
+        make.width.equalTo(self.view.mas_width);
+        make.bottom.equalTo(self.view.mas_bottom);
+    }];
+    
+    [self getListRepairItemOffers];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return m_cellInfos.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    NSString *identifier = cellInfo.cellReuseIdentifier;
+    
+    if ([identifier isEqualToString:@"repairManInfo"])
+    {
+        return [self tableView:tableView repairManInfoCellForIndexPath:indexPath];
+    }
+    else if ([identifier isEqualToString:@"repairItemHeader"])
+    {
+        
+    }
+    else if ([identifier isEqualToString:@"division"])
+    {
+        return [self tableView:tableView divisionForIndexPath:indexPath];
+    }
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.separatorInset = UIEdgeInsetsZero;
+    }
+    
+    cell.textLabel.text = identifier;
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    return cellInfo.cellHeight;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView repairManInfoCellForIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    NSString *identifier = cellInfo.cellReuseIdentifier;
+    
+    MFTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[MFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake((CGRectGetWidth(cell.contentView.frame) - 200) /2 , 0, 200, CGRectGetHeight(cell.contentView.frame));
+        button.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleHeight;
+        [button setTitle:@"查看维修人员详情" forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor hx_colorWithHexString:@"242834"] forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(onClickShowRepairManInfo) forControlEvents:UIControlEventTouchUpInside];
+        [cell.contentView addSubview:button];
+        
+        UIImageView *accessoryImageView = [[UIImageView alloc] initWithImage:MFImage(@"common_btn_next_nor")];
+        cell.accessoryView = accessoryImageView;
+    }
+    return cell;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView divisionForIndexPath:(NSIndexPath *)indexPath
+{
+    MFTableViewCellObject *cellInfo = m_cellInfos[indexPath.row];
+    NSString *identifier = cellInfo.cellReuseIdentifier;
+    
+    MFTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[MFTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    cell.contentView.backgroundColor = [UIColor hx_colorWithHexString:@"f5f5f5"];
+    return cell;
+}
+
+-(void)getListRepairItemOffers
+{
+    __weak typeof(self) weakSelf = self;
+    WDListRepairItemOffersApi *mfApi = [WDListRepairItemOffersApi new];
+    mfApi.diagnoseId = self.diagnoseId;
+    
+    mfApi.animatingView = self.view;
+    [mfApi startWithCompletionBlockWithSuccess:^(YTKBaseRequest * request) {
+        
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        if (!mfApi.messageSuccess) {
+            [strongSelf showTips:mfApi.errorMessage];
+            return;
+        }
+        
+        NSArray *responseNetworkData = mfApi.responseNetworkData;
+        NSMutableArray *tempArray = [NSMutableArray array];
+        for (int i = 0; i < responseNetworkData.count; i++) {
+            WDRepairItemOfferModel *itemModel = [WDRepairItemOfferModel yy_modelWithDictionary:responseNetworkData[i]];
+            [tempArray addObject:itemModel];
+        }
+
+        m_repairItemOffers = tempArray;
+
+        [strongSelf reloadTableView];
+        
+    } failure:^(YTKBaseRequest * request) {
+        
+    }];
+}
+
+-(void)reloadTableView
+{
+    [self makeCellObjects];
+    [m_tableView reloadData];
+}
+
+-(void)makeCellObjects
+{
+    [m_cellInfos removeAllObjects];
+    
+    MFTableViewCellObject *repairManInfo = [MFTableViewCellObject new];
+    repairManInfo.cellHeight = 50.0f;
+    repairManInfo.cellReuseIdentifier = @"repairManInfo";
+    [m_cellInfos addObject:repairManInfo];
+    
+    MFTableViewCellObject *division = [MFTableViewCellObject new];
+    division.cellHeight = 15.0f;
+    division.cellReuseIdentifier = @"division";
+    [m_cellInfos addObject:division];
+    
+    for (int i = 0; i < m_repairItemOffers.count; i++) {
+        
+        MFTableViewCellObject *repairItemHeader = [MFTableViewCellObject new];
+        repairItemHeader.cellHeight = 60.0f;
+        repairItemHeader.cellReuseIdentifier = @"repairItemHeader";
+        repairItemHeader.attachIndex = i;
+        [m_cellInfos addObject:repairItemHeader];
+        
+        MFTableViewCellObject *division = [MFTableViewCellObject new];
+        division.cellHeight = 15.0f;
+        division.cellReuseIdentifier = @"division";
+        [m_cellInfos addObject:division];
+    }
+}
+
+-(void)onClickShowRepairManInfo
+{
+    [self showTips:@"查看维修人员详情"];
 }
 
 - (void)didReceiveMemoryWarning {
