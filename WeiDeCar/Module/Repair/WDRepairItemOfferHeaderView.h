cf7b5fbad5a9eb8814c290dfe5fccb67bd2f764b
mafanghua <980992720@qq.com>
2018-02-10 22:58:27 +0800

    no message

---
+++ WeiDeCar/Module/Repair/WDRepairItemOfferHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  WDRepairItemOfferHeaderView.h
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/10.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIBridgeView.h"
+
+@interface WDRepairItemOfferHeaderView : MMUIBridgeView
+
+@end
---
+++ WeiDeCar/Module/Repair/WDRepairItemOfferHeaderView.m
@@ -0,0 +1,25 @@
+//
+//  WDRepairItemOfferHeaderView.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/10.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDRepairItemOfferHeaderView.h"
+
+@interface WDRepairItemOfferHeaderView ()
+{
+    __weak IBOutlet UILabel *m_createLabel;
+    __weak IBOutlet UILabel *m_carOwnerIdLabel;
+    __weak IBOutlet UILabel *m_mechanicIdLabel;
+    __weak IBOutlet UILabel *m_expertIdLabel;
+    __weak IBOutlet UILabel *m_statusNameLabel;
+}
+
+@end
+
+@implementation WDRepairItemOfferHeaderView
+
+
+@end
