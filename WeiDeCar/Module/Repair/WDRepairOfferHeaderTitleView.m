25f6580097976a1c679b82e18480bd484b92a7f5
mafanghua <980992720@qq.com>
2018-02-10 23:25:56 +0800

    no message

---
+++ WeiDeCar/Module/Repair/WDRepairOfferHeaderTitleView.m
@@ -0,0 +1,21 @@
+//
+//  WDRepairOfferHeaderTitleView.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/10.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDRepairOfferHeaderTitleView.h"
+
+@interface WDRepairOfferHeaderTitleView ()
+{
+    
+}
+
+@end
+
+@implementation WDRepairOfferHeaderTitleView
+
+
+@end
