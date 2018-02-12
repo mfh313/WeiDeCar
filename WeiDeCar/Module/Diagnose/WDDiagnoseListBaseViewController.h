11fefb401f340b3c9ba989cb2ca7f7a01b8f26a8
mafanghua <980992720@qq.com>
2018-02-12 23:00:30 +0800

    no message

---
+++ WeiDeCar/Module/Diagnose/WDDiagnoseListBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  WDDiagnoseListBaseViewController.h
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/12.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIViewController.h"
+
+@interface WDDiagnoseListBaseViewController : MMUIViewController
+
+@end
---
+++ WeiDeCar/Module/Diagnose/WDDiagnoseListBaseViewController.m
@@ -0,0 +1,26 @@
+//
+//  WDDiagnoseListBaseViewController.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/2/12.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDDiagnoseListBaseViewController.h"
+
+@interface WDDiagnoseListBaseViewController ()
+
+@end
+
+@implementation WDDiagnoseListBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
