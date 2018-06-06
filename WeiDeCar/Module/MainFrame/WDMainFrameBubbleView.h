dcc2b2f534868c7489321018798e14e56f5c7cde
mafanghua <980992720@qq.com>
2018-06-07 00:10:59 +0800

    no message

---
+++ WeiDeCar/Module/MainFrame/WDMainFrameBubbleView.h
@@ -0,0 +1,17 @@
+//
+//  WDMainFrameBubbleView.h
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/6/7.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDMainFrameBubbleView : UIView
+{
+    UIView *m_bubbleView;
+    UIView *m_centerView;
+}
+
+@end
---
+++ WeiDeCar/Module/MainFrame/WDMainFrameBubbleView.m
@@ -0,0 +1,15 @@
+//
+//  WDMainFrameBubbleView.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/6/7.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDMainFrameBubbleView.h"
+
+@implementation WDMainFrameBubbleView
+
+
+
+@end
