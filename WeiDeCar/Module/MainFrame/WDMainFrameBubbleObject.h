1663c3f29c57bf9bdf192e25e5e43ec91951b751
mafanghua <980992720@qq.com>
2018-06-07 00:17:31 +0800

    no message

---
+++ WeiDeCar/Module/MainFrame/WDMainFrameBubbleObject.h
@@ -0,0 +1,19 @@
+//
+//  WDMainFrameBubbleObject.h
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/6/7.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WDMainFrameBubbleObject : NSObject
+
+@property (nonatomic,strong) NSString *title;
+@property (nonatomic,assign) BOOL left;
+@property (nonatomic,assign) BOOL bigPoint;
+@property (nonatomic,assign) CGSize bubbleSize;
+@property (nonatomic,strong) NSString *bubbleImage;
+
+@end
--- WeiDeCar/Module/MainFrame/WDMainFrameBubbleObject.m
+++ WeiDeCar/Module/MainFrame/WDMainFrameBubbleObject.m
@@ -1,18 +1,13 @@
 //
-//  WDMainFrameBubbleView.h
+//  WDMainFrameBubbleObject.m
 //  WeiDeCar
 //
 //  Created by mafanghua on 2018/6/7.
 //  Copyright © 2018年 mafanghua. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "WDMainFrameBubbleObject.h"
 
-@interface WDMainFrameBubbleView : UIView
-{
-    UIView *m_bubbleView;
-    UIView *m_centerView;
-}
+@implementation WDMainFrameBubbleObject
 
 @end
