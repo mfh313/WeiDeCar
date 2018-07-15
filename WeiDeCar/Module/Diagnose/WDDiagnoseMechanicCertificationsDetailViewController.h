aaeb5513676bdd742575fc99d533d73b0bff8ce5
mafanghua <980992720@qq.com>
2018-07-15 21:24:55 +0800

    no message

---
+++ WeiDeCar/Module/Diagnose/WDDiagnoseMechanicCertificationsDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  WDDiagnoseMechanicCertificationsDetailViewController.h
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/7/15.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIViewController.h"
+
+@interface WDDiagnoseMechanicCertificationsDetailViewController : MMUIViewController
+
+@property (nonatomic,strong) NSString *diagnoseId;
+
+@end
---
+++ WeiDeCar/Module/Diagnose/WDDiagnoseMechanicCertificationsDetailViewController.m
@@ -0,0 +1,25 @@
+//
+//  WDDiagnoseMechanicCertificationsDetailViewController.m
+//  WeiDeCar
+//
+//  Created by mafanghua on 2018/7/15.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDDiagnoseMechanicCertificationsDetailViewController.h"
+
+@interface WDDiagnoseMechanicCertificationsDetailViewController ()
+
+@end
+
+@implementation WDDiagnoseMechanicCertificationsDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
