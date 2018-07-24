eab3a72445e22d56cd341bc04d2fa756a3a500a6
mafanghua <980992720@qq.com>
2018-07-24 19:03:37 +0800

    no message

---
+++ WeiDeCar/Module/RepairFactories/WDRepairFactoriesDistributionViewController.h
@@ -0,0 +1,13 @@
+//
+//  WDRepairFactoriesDistributionViewController.h
+//  WeiDeCar
+//
+//  Created by EEKA on 2018/7/24.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "MMUIViewController.h"
+
+@interface WDRepairFactoriesDistributionViewController : MMUIViewController
+
+@end
---
+++ WeiDeCar/Module/RepairFactories/WDRepairFactoriesDistributionViewController.m
@@ -0,0 +1,25 @@
+//
+//  WDRepairFactoriesDistributionViewController.m
+//  WeiDeCar
+//
+//  Created by EEKA on 2018/7/24.
+//  Copyright © 2018年 mafanghua. All rights reserved.
+//
+
+#import "WDRepairFactoriesDistributionViewController.h"
+
+@interface WDRepairFactoriesDistributionViewController ()
+
+@end
+
+@implementation WDRepairFactoriesDistributionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
